f49caf427ffbe59375d33981fc74f180d5b442a4
jeisoneduardopineda-beep <jeisoneduardopineda@gmail.com>
2025-08-19 11:02:30 -0500

    Update verify_hash.py

--- verify_hash.py
+++ verify_hash.py
@@ -1,6 +1,6 @@
 import bcrypt
 
 password_plain = b"jeison1411"  # <- tu clave en texto
-stored_hash   = b"$2b$12$MRbdtEWCTQ3oDNojUjz85uUCD15QOLr1KXy6Zl2NlZDDQbaTxruj"  # <- el hash de config.yaml
+stored_hash   = b"$2b$12$PWb1bkGeHYRX2YInizX2P.4.4.fkxCScxAuZzLnz3mTwrFW.qqYEC"  # <- el hash de config.yaml
 
 print("Coincide?:", bcrypt.checkpw(password_plain, stored_hash))
