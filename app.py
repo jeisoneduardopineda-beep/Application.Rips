697e9b02fa5df3929df8c3517a187dca77c8c64c
jeisoneduardopineda-beep <jeisoneduardopineda@gmail.com>
2026-02-05 17:34:08 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -241,7 +241,7 @@ CAMPOS_CODIGOS = [
     "tipoUsuario", "viaIngresoServicioSalud", "modalidadGrupoServicioTecSal",
     "grupoServicios", "finalidadTecnologiaSalud", "conceptoRecaudo",
     "tipoMedicamento", "tipoOS", "codZonaTerritorialResidencia", "codMunicipioResidencia",
-    "codPaisResidencia", "codPaisOrigen","numAutorizacion","codPrestador","codProcedimiento","numDocumentoIdentificacion","condicionDestinoUsuarioEgreso"
+    "codPaisResidencia", "codPaisOrigen","numAutorizacion","codPrestador","codProcedimiento","numDocumentoIdentificacion","condicionDestinoUsuarioEgreso","tipoDiagnosticoPrincipal"
 ]
 
 # ★ CLAVES DE FECHA Y FECHA-HORA (ajusta si agregas nuevas)
@@ -607,6 +607,7 @@ def main():
 # 6) BOOT CON AIRBAG
 # ──────────────────────────────────────────────────────────────────────────────
 guard(main)
+
 
 
 
